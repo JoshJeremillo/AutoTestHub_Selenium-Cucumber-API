fc0260d89fe1033a4919b75f969b9db9fd42179c
Joshua Jeremillo <39651546+JoshJeremillo@users.noreply.github.com>
2025-11-15 01:28:42 +0400

    Update SampleSteps.java

--- src/test/java/com/SampleAutomation/steps/SampleSteps.java
+++ src/test/java/com/SampleAutomation/steps/SampleSteps.java
@@ -1,28 +1,40 @@
 package com.example.steps;
 
-
 import io.cucumber.java.en.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import static org.junit.Assert.assertEquals;
-
 
 public class SampleSteps {
 
+    private WebDriver driver;
 
-WebDriver driver;
+    @Given("I open Google")
+    public void i_open_google() {
+        // Automatically setup ChromeDriver
+        WebDriverManager.chromedriver().setup();
 
+        // Configure Chrome to run headless (required for Codespaces / CI environments)
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");           // Run without GUI
+        options.addArguments("--no-sandbox");         // Linux requirement
+        options.addArguments("--disable-dev-shm-usage"); // Reduce resource usage
 
-@Given("I open Google")
-public void i_open_google() {
-driver = new ChromeDriver();
-driver.get("https://www.google.com");
-}
+        // Initialize WebDriver
+        driver = new ChromeDriver(options);
 
+        // Open Google homepage
+        driver.get("https://www.google.com");
+    }
 
-@Then("the title should be {string}")
-public void the_title_should_be(String expectedTitle) {
-assertEquals(expectedTitle, driver.getTitle());
-driver.quit();
-}
+    @Then("the title should be {string}")
+    public void the_title_should_be(String expectedTitle) {
+        // Verify the page title
+        assertEquals(expectedTitle, driver.getTitle());
+
+        // Close the browser
+        driver.quit();
+    }
 }
