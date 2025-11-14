e390f1ecd858eeb16d715804911b85b1105a5910
Joshua Jeremillo <39651546+JoshJeremillo@users.noreply.github.com>
2025-11-15 01:07:13 +0400

    Create TestRunner.java

---
+++ src/test/java/com/SampleAutomation/runners/TestRunner.java
@@ -0,0 +1,16 @@
+package com.example.runners;
+
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+features = "features",
+glue = "com.example.steps",
+plugin = {"pretty", "html:target/cucumber-reports.html"},
+monochrome = true
+)
+public class TestRunner {}
